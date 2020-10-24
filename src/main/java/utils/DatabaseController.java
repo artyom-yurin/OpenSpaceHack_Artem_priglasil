99b478a40f873026bd5c14d63b1241a43dc4458e
Igor Krasheninnikov <igor.krash@gmail.com>
2020-10-24 05:11:15 +0300

    + index
    Signed-off-by: Igor Krasheninnikov <igor.krash@gmail.com>

--- src/main/java/utils/DatabaseController.java
+++ src/main/java/utils/DatabaseController.java
@@ -1,13 +1,24 @@
 package utils;
 
+import com.google.gson.Gson;
+import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+import utils.RequestBody;
+
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+
 
 public class DatabaseController {
     //  Database credentials
     static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/questions";
     static final String USER = "artem_priglasil";
     static final String PASS = "4r73m_pr1gl451l";
-    private Connection connection;
+    private java.sql.Connection connection;
+    Gson gson = new Gson();
+    MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     public boolean establishConnection() {
         try {
@@ -29,7 +40,7 @@ public class DatabaseController {
         return this.connection != null;
     }
 
-    private void prepareDb() {
+    public void prepareDb() {
         String prep_query = "DROP TABLE IF EXISTS knowledge_base, vectors";
         try (PreparedStatement prSt = connection.prepareStatement(prep_query)) {
             prSt.executeUpdate();
@@ -39,8 +50,7 @@ public class DatabaseController {
     }
 
     public boolean initDb() {
-        prepareDb();
-        String table_create = "CREATE TABLE IF NOT EXISTS knowledge_base(id SERIAL, " +
+        String table_create = "CREATE TABLE IF NOT EXISTS knowledge_base(id SERIAL PRIMARY KEY, " +
                 "kb_id VARCHAR(256)," +
                 "request TEXT NOT NULL, " +
                 "request_type TEXT, " +
@@ -68,11 +78,36 @@ public class DatabaseController {
         } catch (Exception e) {
             System.out.println("Database initialization failed");
         }
+        String index_db = "CREATE TABLE IF NOT EXISTS vectors(" +
+                "id INT PRIMARY KEY REFERENCES knowledge_base(id)," +
+                "vector FLOAT[])";
+        try (PreparedStatement prSt = connection.prepareStatement(index_db)) {
+            prSt.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Database initialization failed" + e.toString());
+        }
         String fill_db = "COPY knowledge_base(kb_id, request, request_type, dbo_type, question, video_link, faq_link, use_link, step1, step2, step3, step4, step5, step6, step7, step8, step9, step10, step11, step12, step13, step14) FROM '/app/KB.csv' DELIMITER ',' CSV HEADER";
         try (PreparedStatement prSt = connection.prepareStatement(fill_db)) {
             prSt.executeUpdate();
         } catch (Exception e) {
             System.out.println("Database filling failed");
+        }
+        return true;
+    }
+
+    public boolean index() throws IOException, JSONException, SQLException {
+        Statement get_questions = connection.createStatement();
+        ResultSet questions = get_questions.executeQuery("SELECT id, question FROM knowledge_base");
+        while (questions.next()) {
+            System.out.println(questions.getString("question"));
+            ArrayList<String> texts = new ArrayList<>();
+            texts.add(questions.getString("question"));
+            RequestBody request = new utils.RequestBody("121", texts, false);
+            String jsonStr = gson.toJson(request);
+            okhttp3.RequestBody body = okhttp3.RequestBody.create(jsonStr, this.JSON);
+            String response = RequestBody.make_post_request("http://10.241.1.243:8125/encode ", body);
+            JSONObject responseObj = new JSONObject(response);
+            System.out.println(responseObj.toString());
         }
         return true;
     }
--- src/main/java/utils/DatabaseTest.java
+++ src/main/java/utils/DatabaseTest.java
@@ -1,11 +1,18 @@
 package utils;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class DatabaseTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JSONException, SQLException, IOException {
         DatabaseController controller = new DatabaseController();
         if (controller.establishConnection()) {
             System.out.println("Database connected");
         }
+        controller.prepareDb();
         controller.initDb();
+        controller.index();
     }
 }
